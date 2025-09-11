90f9f8e8291891bf024bceb69c0063310dee98b3
Lucorrea977 <luucamila6@gail.com>
2025-09-11 15:08:58 -0300

    delete

--- smartbiz/client/src/components/Nav.jsx
+++
@@ -1,8 +0,0 @@
-export default function Nav() {
-  return (
-    <nav className="bg-blue-600 text-white p-3 flex justify-between">
-      <h1 className="font-bold text-xl">SmartBiz</h1>
-      <button className="bg-white text-blue-600 px-2 rounded">Salir</button>
-    </nav>
-  );
-}
